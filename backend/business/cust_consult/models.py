f1925787f9519935eb4ac7e8329cbd6cd84d3eea
양정현 <maudl0ve62@gmail.com>
2025-09-11 10:16:58 +0900

    model.py 완료

--- backend/business/cust_consult/models.py
+++ backend/business/cust_consult/models.py
@@ -11,4 +11,13 @@ class Consult(models.Model):
         db_table = 'consult'
         managed = False
 
-class Consult_stage(models.Model)
+class Consult_stage(models.Model):
+    id= models.AutoField(primary_key=True) # INT / PK,Auto_Increment / 상담단계 ID
+    consult_id= models.IntegerField() # INT / FK > consult_id / 상담 정보 ID(consult.id)
+    stage_meta_id= models.IntegerField() # INT / FK > sales_stage_meta.id / 영업 단계 ID(sales_stage_meta.id)
+    stage_name= models.CharField(max_length=50) # VARCHAR(50) / Not null / 단계명 (스냅샷 저장)
+    created_at= models.DateTimeField() # Timestamp / Default now() / 생성 시각
+
+    class Meta:
+        db_table = 'consult_stage'
+        managed = False
