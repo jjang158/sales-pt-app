5b006002616c176395b57a09f5ee270119459b0e
jjang158 <68089004+jjang158@users.noreply.github.com>
2025-09-16 15:02:09 +0900

    Delete backend/business/view_todo/urls.py

--- backend/business/view_todo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('/', views.todo, name='todo'),
-    path('/<int:id>', views.todo, name='todo'),
-    path('/complete', views.todo_complete, name="todo_complete")
-]
